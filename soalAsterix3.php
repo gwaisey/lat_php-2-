94bd31958bc032c354b13a6d079243d2c35fa200
gwaisey <gracemaegozali@gmail.com>
2026-03-06 14:13:31 +0700

    feat: logic fix soal asterix 2

--- soalAsterix3.php
+++ soalAsterix3.php
@@ -1,16 +1,16 @@
 <?php
-$n = 4;
+$n = 5;
 echo "<pre>";
 
-for($i = 0; $i<$n; $i++){
-    for($j = 0; $j<$i; $j++){
+for($i = 0; $i < $n; $i++){
+    for($j = 0; $j < $i; $j++){
         echo "&nbsp;&nbsp;";
     }
 
     for($k = $n; $k>$i; $k--){
         echo "* ";
     }
-    echo "\n";
+    echo "<br>";
 }
 echo "</pre>";
 ?>
